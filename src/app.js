173c21a26f27f19d279f468fc4952c18280be9ba
Romesh Yadav <romesh.yadav@jumbaya.com>
2024-09-21 19:04:57 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -2,27 +2,27 @@ const express = require('express');
 
 const app = express();
 
-// order of the routes will make a difference - it will try to match the route from top to bottom
-// if you are using app.use() then it will match the routes string
-// and if after `/` is there any thing it will still go that route only
-// if you are using app.get() then it will match exactly to that string
-
-// ab?c: Matches ac or abc (zero or one b).
-// ab+c: Matches abc, abbc, abbbc, etc. (one or more bs).
-// ab*c: Matches ac, abc, abbc, etc. (zero or more bs or any thing between).
-// a(bc)?d: Means bc is optional === ad or abcd
-
-// user?userId=101 => means you are sending something in query
-//    you can use it as req.query
-
-// Dynamic params (colon :)
-// user/101
-// user/:userId => means you are sending something in params
-//    you can use it as req.params
-
-app.get('/user', (req, res) => {
-  res.send({ firstName: 'Romesh', lastName: 'Yadav' });
-});
+app.use(
+  '/user',
+  (req, res, next) => {
+    // this function is called route handler
+    // we need to send response otherwise it will stuck in infinite loop
+    console.log('Handling the route handler');
+    // res.send('Response !!');
+    // if there is no response is send and not using next() function than again it will stuck into the loop
+    // if we use next() without sending the res.send() from this than it will call the next route handler
+    // if we use next() with res.send() than res will be send from 1st route handler which is written first and the log will print from both 1st and 2nd
+    // but we will get an error `Cannot set headers after they are sent to the client` basically we can not resend the response
+    next();
+    // res.send('Response !!');
+  },
+  (req, res) => {
+    // we can send multiple route handlers
+    console.log('Handling the route handler 2!!');
+    res.send('2nd Response !!');
+  },
+  // there should be res.send in the last route handler and there should not be next() function call in last handler
+);
 
 app.listen(3000, () => {
   console.log('server is successfully listening on port 3000');
