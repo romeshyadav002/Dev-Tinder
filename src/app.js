5968172552397c92bc72e4077900ab97cf8bf8c4
Romesh Yadav <romesh.yadav@jumbaya.com>
2024-09-22 18:39:12 +0530

    API level data sanitization

--- src/app.js
+++ src/app.js
@@ -64,10 +64,27 @@ app.delete('/user', async (req, res) => {
 });
 
 // Update a user
-app.patch('/user', async (req, res) => {
-  const userId = req.body.userId;
+app.patch('/user/:userId', async (req, res) => {
+  const userId = req.params?.userId;
   const data = req.body;
   try {
+    const ALLOWED_UPDATES = [
+      // 'userId',
+      'photoUrl',
+      'about',
+      'gender',
+      'age',
+      'skills',
+    ];
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k),
+    );
+    if (!isUpdateAllowed) {
+      throw new Error('Update not allowed');
+    }
+    if (data?.skills.length > 0) {
+      throw new Error('Skills cannot be more than 10');
+    }
     const user = await User.findByIdAndUpdate({ _id: userId }, data, {
       returnDocument: 'before',
       runValidators: true,
